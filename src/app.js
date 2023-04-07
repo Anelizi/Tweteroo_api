da018a22be7a5624bdc676666952d7b34bb5c258
Anelizi <anelizicarvalho@gmail.com>
2023-04-07 19:33:10 -0300

    feat: Add post in sign-up

--- src/app.js
+++ src/app.js
@@ -1,13 +1,39 @@
-import express from 'express';
-
-const PORT = 5000;
-const user = [];
-const tweet = [];
+import express from "express";
+import cors from "cors";
 
 const app = express();
+app.use(cors());
+app.use(express.json());
+const PORT = 5000;
+
+const user = [
+  {
+    username: "bobesponja",
+    avatar:
+      "https://cdn.shopify.com/s/files/1/0150/0643/3380/files/Screen_Shot_2019-07-01_at_11.35.42_AM_370x230@2x.png",
+  },
+];
+
+const tweet = [
+  {
+    username: "bobesponja",
+    tweet: "Eu amo hambúrguer de siri!",
+  },
+];
+
+app.post("/sign-up", (req, res) => {
+    const { username, avatar} = req.body;
+    console.log(req.body)
+    const users ={
+        username,
+        avatar
+    }
+    user.push(users);
+    res.send("OK");
+});
 
 app.get("/tweets", (req, res) => {
-    res.send("oi")
-})
+  res.send(tweet);
+});
 
 app.listen(PORT, () => console.log(`Servidor rodando na porta ${PORT}`));
