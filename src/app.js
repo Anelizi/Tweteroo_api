f6628c3240ccd6ab0adda749a5e99879a11fad4c
Anelizi <anelizicarvalho@gmail.com>
2023-04-07 22:26:03 -0300

    feat: Add post in tweets

--- src/app.js
+++ src/app.js
@@ -4,36 +4,52 @@ import cors from "cors";
 const app = express();
 app.use(cors());
 app.use(express.json());
+
 const PORT = 5000;
+let userLocalhost;
 
-const user = [
-  {
-    username: "bobesponja",
-    avatar:
-      "https://cdn.shopify.com/s/files/1/0150/0643/3380/files/Screen_Shot_2019-07-01_at_11.35.42_AM_370x230@2x.png",
-  },
-];
-
-const tweet = [
-  {
-    username: "bobesponja",
-    tweet: "Eu amo hambúrguer de siri!",
-  },
-];
+const users = [];
+const tweets = [];
 
 app.post("/sign-up", (req, res) => {
-    const { username, avatar} = req.body;
-    console.log(req.body)
-    const users ={
-        username,
-        avatar
-    }
-    user.push(users);
-    res.send("OK");
+  const { username, avatar } = req.body;
+  console.log(req.body);
+  userLocalhost = req.body;
+
+  if(!username || !avatar){
+    return res.status(400).send("BAD REQUEST");
+  }
+
+  const user = {
+    username,
+    avatar,
+  };
+
+  users.push(user);
+  res.status(200).send("OK");
+});
+
+app.post("/tweets", (req, res) => {
+  const { username, tweet } = req.body;
+  console.log(req.body);
+
+  if (!username || !tweet) {
+    return res.status(400).send("BAD REQUEST");
+  }
+
+  if (!userLocalhost){
+    return res.status(401).send("UNAUTHORIZED")
+  }
+
+  const newTweets = { ...req.body, avatar: userLocalhost.avatar };
+
+  tweets.push(newTweets);
+  res.status(200).send("OK");
 });
 
 app.get("/tweets", (req, res) => {
-  res.send(tweet);
+  const tweetsLast = [...tweets].reverse().slice(0,[10])
+  res.send(tweetsLast);
 });
 
 app.listen(PORT, () => console.log(`Servidor rodando na porta ${PORT}`));
